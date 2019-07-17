7a6af38adf76dcd0341c9367ea4369dcff05caad
yasincukur <cukurr@hotmail.com>
2019-07-17 10:32:14 +0300

    ikinci commit

--- WFA_HataYonetimi/Form1.Designer.cs
+++ WFA_HataYonetimi/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -48,11 +50,31 @@
             this.textBox1.Size = new System.Drawing.Size(196, 20);
             this.textBox1.TabIndex = 1;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(106, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(183, 88);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "hata yonetimsiz";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(106, 293);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 86);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "hata yonetimli mesaj";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -66,6 +88,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
